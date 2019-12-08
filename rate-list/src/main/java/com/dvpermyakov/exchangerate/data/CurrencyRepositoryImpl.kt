44e886ad95c49b9392cc31f7eba6cc7177d7b5f9
dpermyakov <dvpermyakov1@gmail.com>
2019-12-08 20:26:07 +0300

    add currencies

--- rate-list/src/main/java/com/dvpermyakov/exchangerate/data/CurrencyRepositoryImpl.kt
+++ rate-list/src/main/java/com/dvpermyakov/exchangerate/data/CurrencyRepositoryImpl.kt
@@ -24,6 +24,42 @@ class CurrencyRepositoryImpl : CurrencyRepository {
                 image = "file:///android_asset/usa.png",
                 code = CurrencyCode("USD"),
                 name = "Доллар"
+            ),
+            CurrencyEntity(
+                id = 4,
+                image = "file:///android_asset/chf.png",
+                code = CurrencyCode("CHF"),
+                name = "Швейцарский франк"
+            ),
+            CurrencyEntity(
+                id = 5,
+                image = "file:///android_asset/sek.png",
+                code = CurrencyCode("SEK"),
+                name = "Шведская крона"
+            ),
+            CurrencyEntity(
+                id = 6,
+                image = "file:///android_asset/jpy.png",
+                code = CurrencyCode("JPY"),
+                name = "Иена"
+            ),
+            CurrencyEntity(
+                id = 7,
+                image = "file:///android_asset/czk.png",
+                code = CurrencyCode("CZK"),
+                name = "Чешская крона"
+            ),
+            CurrencyEntity(
+                id = 8,
+                image = "file:///android_asset/thb.png",
+                code = CurrencyCode("THB"),
+                name = "Тайский бат"
+            ),
+            CurrencyEntity(
+                id = 9,
+                image = "file:///android_asset/aud.png",
+                code = CurrencyCode("AUD"),
+                name = "Австралийский доллар"
             )
         )
     }
